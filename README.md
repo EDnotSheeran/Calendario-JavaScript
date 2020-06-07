c1450a06315489f8b51faca24889d649a6669db1
EDnotSheeran <juninho.tab3@gmail.com>
2020-06-07 00:17:32 -0300

    suporte a mudanca de linguagem

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Calendario-JavaScript
 
 [Visualizar](https://ednotsheeran.github.io/Calendario-JavaScript/.)
+
+
+# Mude a liguagem do calendario pelo atributo language (letras maiusculas = nome inteiro)
+`<div id="calendar" language="PT-BR">`
--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let today = new Date()
+let calendar = document.querySelector('#calendar')
+let language = calendar.attributes.getNamedItem('language').value
+let today = new Date() 
 let currentMonth = today.getMonth()
 let currentYear = today.getFullYear()
 let currentDay = today.getDate()
-
-let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-
+let months = getMonthsNames(language)
 let monthAndYear = document.querySelector('#monthAndYear')
 
+// Constroi o Calendario
 showCalendar(currentMonth, currentYear)
 
 function showCalendar(month, year) {
+    showCalendarHeader()
     let firstDay = new Date(year, month).getDay()// retorna o dia da semana
     let daysInMonth = 32 - new Date(year, month, 32).getDate()
 
@@ -48,19 +50,62 @@ function showCalendar(month, year) {
                 date++
             }
         }
-
         tbl.appendChild(row)
     }
 }
-
+// Volta para o mes anterior
 function previous() {
     currentYear = currentMonth === 0 ? currentYear -1 : currentYear
     currentMonth = currentMonth === 0 ? 11: currentMonth - 1
     showCalendar(currentMonth, currentYear)
 }
-
+// Vai para o proximo mes
 function next() {
     currentYear = currentMonth === 11 ? currentYear + 1 : currentYear
     currentMonth = (currentMonth + 1) % 12 
     showCalendar(currentMonth, currentYear) 
+}
+// Retorna o nome dos meses
+function getMonthsNames(laguage){
+    switch (laguage) {
+        case 'pt-br':
+            return ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
+            break;
+        case 'PT-BR':
+            return ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+            break;
+        default:
+            return ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+            break;
+    }
+}
+// Insere os dias da semana no calendario
+function showCalendarHeader(){
+    let tbl = document.querySelector('#calendar-head')
+
+    tbl.innerHTML = ''
+
+    let row = document.createElement('tr')
+    let daysOfWeek = getDaysOfWeeksNames(language)
+    for(let k = 0; k < 7; k++){
+        let cell = document.createElement('th')
+        let cellText = document.createTextNode(daysOfWeek[k])
+        cell.appendChild(cellText)
+        row.appendChild(cell)
+    }
+    tbl.appendChild(row)
+}
+// Retorna os nome dos dias da semana
+function getDaysOfWeeksNames(language){
+    switch (language) {
+        case ('pt-br'):
+            return  ["Dom", "Seg", "Ter" ,"Qua", "Qui", "Sex", "Sab"]
+            break;
+        case ('PT-BR'):
+            return  ["Dom", "Seg", "Ter" ,"Qua", "Qui", "Sex", "Sab"]
+            break;
+        default:
+            return ["Sun", "Mon", "Tue" ,"Wed", "Thu", "Fri", "Sat"]
+            break;
+    }
 }
